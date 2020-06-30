d958de64c46f5a81640d38527932fecdfc873f41
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-06-30 14:48:38 +0200

    Implement delegate to load quantum circuit code

--- process-model/src/main/java/org/quantil/quantme/simon/tasks/LoadQuantumCircuitDelegate.java
+++ process-model/src/main/java/org/quantil/quantme/simon/tasks/LoadQuantumCircuitDelegate.java
@@ -1,5 +1,12 @@
 package org.quantil.quantme.simon.tasks;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.slf4j.Logger;
@@ -10,7 +17,29 @@ public class LoadQuantumCircuitDelegate implements JavaDelegate {
 	private final static Logger LOGGER = LoggerFactory.getLogger(LoadQuantumCircuitDelegate.class);
 
 	public void execute(DelegateExecution execution) throws Exception {
-		LOGGER.info("Loading quantum circuit from specified URL: {}", execution.getVariable("circuitURL"));
-		// TODO		
+		String urlVariable = execution.getVariable("URL").toString();
+		LOGGER.info("Loading quantum circuit from specified URL: {}", urlVariable);
+		
+		URL url = null;
+		try {
+			url = new URL(urlVariable);
+		} catch (MalformedURLException e) {
+			LOGGER.error("Failed to parse url process variable to URL");
+			throw new BpmnError("URL to retrieve quantum circuit is not valid!");
+		}
+		
+		try (InputStream inputStream = url.openStream()){
+			byte[] circuitBytes = IOUtils.toByteArray(inputStream);
+			LOGGER.info("Successfully downloaded quantum circuit from URL with size: {} bytes", circuitBytes.length);
+			
+			// Set variables of the outgoing quantum data object
+			execution.setVariableLocal("CircuitId", 1);
+			execution.setVariableLocal("QuantumCircuit", circuitBytes);
+			execution.setVariableLocal("ProgrammingLanguage", "Qiskit");
+			execution.setVariableLocal("ExecutionResult", null);
+		} catch (IOException e) {
+			LOGGER.error("Failed to load circuit from URL: {}", urlVariable);
+			throw new BpmnError("Failed to load circuit from URL!");
+		}	
 	}
 }
