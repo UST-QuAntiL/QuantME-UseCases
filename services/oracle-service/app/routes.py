56812bca64559cc3a810e1fc78aeb4ed22f2f7e2
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-07-01 18:26:21 +0200

    Load oracle circuit from given url

--- services/oracle-service/app/routes.py
+++ services/oracle-service/app/routes.py
@@ -21,6 +21,7 @@ from app import app
 from flask import jsonify, abort, request
 from qiskit import transpile
 from qiskit.transpiler.exceptions import TranspilerError
+from urllib import request as urllib_request
 import logging
 import json
 import base64
@@ -65,6 +66,7 @@ def expand_oracle():
     if not 'OracleCircuitUrl' in request.json:
         app.logger.error("OracleCircuitUrl not defined in request")
         abort(400, "OracleCircuitUrl not defined in request")
+    oracle_url = request.json['OracleCircuitUrl']
 
     if not 'QuantumCircuit' in request.json:
         app.logger.error("QuantumCircuit not defined in request")
@@ -79,12 +81,11 @@ def expand_oracle():
     with open(os.path.join(temp_dir, file_name + ".py"), "wb") as f:
         f.write(quantum_circuit_bytes)
     sys.path.append(temp_dir)
-
     app.logger.info("Created file at " + os.path.join(temp_dir, file_name + ".py"))
 
-    # get circuit as code
-    mod = importlib.import_module(file_name)
-    circuit_code = mod.get_circuit()
+    # get quantum circuit as code
+    circuit_mod = importlib.import_module(file_name)
+    circuit_code = circuit_mod.get_circuit()
 
     # extract classical and quantum register for manipulations
     if len(circuit_code.qubits) == 0:
@@ -93,11 +94,38 @@ def expand_oracle():
     if len(circuit_code.clbits) == 0:
         app.logger.error("QuantumCircuit uses no classical bits. Aborting...")
         abort(400, "QuantumCircuit uses no classical bits.")
-    quantum_register = circuit_code.qubits[0].register
-    classical_register = circuit_code.clbits[0].register
-    app.logger.info("QuantumCircuit contains classical register with " + str(classical_register.size) + " bits and quantum register with " + str(quantum_register.size) + " qubits.")
+    circuit_quantum_register = circuit_code.qubits[0].register
+    circuit_classical_register = circuit_code.clbits[0].register
+    app.logger.info("QuantumCircuit contains classical register with " + str(circuit_classical_register.size) + " bits and quantum register with " + str(circuit_quantum_register.size) + " qubits.")
 
-    # TODO: load oracle; manipulate quantum circuit
+    # get oracle circuit from URL
+    app.logger.info("Downloading oracle circuit from URL: " + oracle_url)
+    oracle_file_name = file_name + "_" + str(oracle_Id)
+    with open(os.path.join(temp_dir, oracle_file_name + ".py"), "w") as f:
+        f.write(urllib_request.urlopen(oracle_url).read().decode("utf-8"))
+    sys.path.append(temp_dir)
+    app.logger.info("Created file at " + os.path.join(temp_dir, oracle_file_name + ".py"))
+
+    # get oracle circuit as code
+    try:
+        oracle_mod = importlib.import_module(oracle_file_name)
+        oracle_code = oracle_mod.get_circuit()
+    except:
+        app.logger.error("Error while importing oracle code from given url!")
+        abort(400, "Error while importing oracle code from given url!")
+
+    # extract classical and quantum register for manipulations
+    if len(oracle_code.qubits) == 0:
+        app.logger.error("QuantumCircuit uses no qubits. Aborting...")
+        abort(400, "QuantumCircuit uses no qubits.")
+    oracle_quantum_register = oracle_code.qubits[0].register
+    app.logger.info("OracleCircuit quantum register with " + str(oracle_quantum_register.size) + " qubits.")
+
+    if oracle_quantum_register.size != circuit_quantum_register.size:
+        app.logger.error("Oracle has to operate on the same size of quantum register than the quantum circuit!")
+        abort(400, "Oracle has to operate on the same size of quantum register than the quantum circuit!")
+
+    # TODO: manipulate quantum circuit
 
     sys.path.remove(temp_dir)
     shutil.rmtree(temp_dir, ignore_errors=True)
