07452000cd0cdb834260837c8f77efe09187b9f0
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-07-15 16:47:50 +0200

    Update oracle

--- circuits/oracle2.py
+++ circuits/oracle2.py
@@ -3,10 +3,10 @@ from qiskit import QuantumRegister, QuantumCircuit
 # Truth table:
 # q[1] | q[0] ||| q[3] | q[2]
 #   0  |   0  |||   0  |   0
-#   0  |   1  |||   0  |   1
+#   0  |   1  |||   1  |   0
 #   1  |   0  |||   1  |   0
-#   1  |   1  |||   1  |   1
-# --> searched bit string: s = 00
+#   1  |   1  |||   0  |   0
+# --> searched bit string: s = 11
 
 qc = QuantumCircuit()
 
@@ -14,7 +14,7 @@ q = QuantumRegister(4, 'q')
 
 qc.add_register(q)
 
-qc.cx(q[0], q[2])
+qc.cx(q[0], q[3])
 qc.cx(q[1], q[3])
 
 def get_circuit(**kwargs):
