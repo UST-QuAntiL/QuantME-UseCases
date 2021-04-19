ae87c0fe2459d7aa8afee51512fa59fbca8409dd
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2021-04-19 10:44:05 +0200

    Remove sample workflows from deployed Camunda instance

--- 2021-icws/docker/initialized-winery/tosca-definitions/artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/Camunda_Install_IA_w1/files/install.sh
+++ 2021-icws/docker/initialized-winery/tosca-definitions/artifacttemplates/http%3A%2F%2Fopentosca.org%2Fartifacttemplates/Camunda_Install_IA_w1/files/install.sh
@@ -5,3 +5,6 @@ sudo mkdir camunda >> ~/camunda_install.log
 sudo tar xf camunda-bpm-tomcat-7.14.0.tar.gz -C camunda >> ~/camunda_install.log
 sudo chmod -R 777 camunda >> ~/camunda_install.log
 
+# remove example workflows from the engine
+sudo rm -rf ~/camunda/server/apache-tomcat-9.0.36/webapps/camunda-invoice
+sudo rm -rf ~/camunda/server/apache-tomcat-9.0.36/webapps/camunda-welcome
