559cda403c958ea828b221c5436a59b55d384639
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2022-03-09 10:18:17 +0100

    Simplify SPSAOptimizer

--- TBD/scripts/variational-quantum-support-vector-machine/SPSAOptimizer.py
+++ TBD/scripts/variational-quantum-support-vector-machine/SPSAOptimizer.py
@@ -3,65 +3,65 @@ import math
 
 
 def cross_entropy(predictions, targets, epsilon=1e-12):
-	predictions = np.clip(predictions, epsilon, 1. - epsilon)
-	N = predictions.shape[0]
-	tmp = np.sum(targets*np.log(predictions), axis=1)
-	ce = -np.sum(tmp)/N
-	return ce
+    predictions = np.clip(predictions, epsilon, 1. - epsilon)
+    N = predictions.shape[0]
+    tmp = np.sum(targets * np.log(predictions), axis=1)
+    ce = -np.sum(tmp) / N
+    return ce
 
 
 def cost_estimate(probs, gt_labels, shots=None):
-	mylabels = np.zeros(probs.shape)
-	for i in range(gt_labels.shape[0]):
-		whichindex = gt_labels[i]
-		mylabels[i][whichindex] = 1
+    mylabels = np.zeros(probs.shape)
+    for i in range(gt_labels.shape[0]):
+        whichindex = gt_labels[i]
+        mylabels[i][whichindex] = 1
 
-	x = cross_entropy(probs, mylabels)
-	return x
+    x = cross_entropy(probs, mylabels)
+    return x
 
 
 def assign_label(measured_key, num_classes):
-	measured_key = np.asarray([int(k) for k in list(measured_key)])
-	num_qubits = len(measured_key)
-	if num_classes == 2:
-		if num_qubits % 2 != 0:
-			total = np.sum(measured_key)
-			return 1 if total > num_qubits / 2 else 0
-		else:
-			hamming_weight = np.sum(measured_key)
-			is_odd_parity = hamming_weight % 2
-			return is_odd_parity
+    measured_key = np.asarray([int(k) for k in list(measured_key)])
+    num_qubits = len(measured_key)
+    if num_classes == 2:
+        if num_qubits % 2 != 0:
+            total = np.sum(measured_key)
+            return 1 if total > num_qubits / 2 else 0
+        else:
+            hamming_weight = np.sum(measured_key)
+            is_odd_parity = hamming_weight % 2
+            return is_odd_parity
 
-	elif num_classes == 3:
-		first_half = int(np.floor(num_qubits / 2))
-		modulo = num_qubits % 2
-		# First half of key
-		hamming_weight_1 = np.sum(measured_key[0:first_half + modulo])
-		# Second half of key
-		hamming_weight_2 = np.sum(measured_key[first_half + modulo:])
-		is_odd_parity_1 = hamming_weight_1 % 2
-		is_odd_parity_2 = hamming_weight_2 % 2
+    elif num_classes == 3:
+        first_half = int(np.floor(num_qubits / 2))
+        modulo = num_qubits % 2
+        # First half of key
+        hamming_weight_1 = np.sum(measured_key[0:first_half + modulo])
+        # Second half of key
+        hamming_weight_2 = np.sum(measured_key[first_half + modulo:])
+        is_odd_parity_1 = hamming_weight_1 % 2
+        is_odd_parity_2 = hamming_weight_2 % 2
 
-		return is_odd_parity_1 + is_odd_parity_2
+        return is_odd_parity_1 + is_odd_parity_2
 
-	else:
-		total_size = 2**num_qubits
-		class_step = np.floor(total_size / num_classes)
+    else:
+        total_size = 2 ** num_qubits
+        class_step = np.floor(total_size / num_classes)
 
-		decimal_value = measured_key.dot(1 << np.arange(measured_key.shape[-1] - 1, -1, -1))
-		key_order = int(decimal_value / class_step)
-		return key_order if key_order < num_classes else num_classes - 1
+        decimal_value = measured_key.dot(1 << np.arange(measured_key.shape[-1] - 1, -1, -1))
+        key_order = int(decimal_value / class_step)
+        return key_order if key_order < num_classes else num_classes - 1
 
 
 def return_probabilities(counts, num_classes):
-	probs = np.zeros(((len(counts), num_classes)))
-	for idx in range(len(counts)):
-		count = counts[idx]
-		shots = sum(count.values())
-		for k, v in count.items():
-			label = assign_label(k, num_classes)
-			probs[idx][label] += v / shots
-	return probs
+    probs = np.zeros(((len(counts), num_classes)))
+    for idx in range(len(counts)):
+        count = counts[idx]
+        shots = sum(count.values())
+        for k, v in count.items():
+            label = assign_label(k, num_classes)
+            probs[idx][label] += v / shots
+    return probs
 
 
 class SPSAOptimizer():
@@ -102,37 +102,35 @@ class SPSAOptimizer():
         """
 
         n_results = len(results.results)
-        results_list = False
-
         n_data = len(labels)
         n_classes = len(set(labels))
-        exact_costs = n_results//n_data == 3 # determines whether results contain data for thetas/exact costs computation
+        exact_costs = n_results // n_data == 3  # determines whether results contain data for thetas/exact costs computation
 
         results_curr = results if exact_costs else None
         results_plus, results_minus = results, results
         indices_curr, indices_plus, indices_minus = None, None, None
-        if (exact_costs and results_list):
-            results_curr, results_plus, results_minus = np.array_split(results, 3)
-        elif (results_list):
-            results_plus, results_minus = np.array_split(results, 2)
-        elif (exact_costs):
-            indices_curr, indices_plus, indices_minus = np.array_split(range(3*n_data), 3)
+        if (exact_costs):
+            indices_curr, indices_plus, indices_minus = np.array_split(range(3 * n_data), 3)
         else:
-            indices_plus, indices_minus = np.array_split(range(2*n_data), 2)
+            indices_plus, indices_minus = np.array_split(range(2 * n_data), 2)
 
-        probs_plus, pred_lbls_plus = cls.computeProbabilities(results_plus, is_statevector, n_data, n_classes, indices=indices_plus)
-        probs_minus, pred_lbls_minus = cls.computeProbabilities(results_minus, is_statevector, n_data, n_classes, indices=indices_minus)
+        probs_plus, pred_lbls_plus = cls.computeProbabilities(results_plus, is_statevector, n_data, n_classes,
+                                                              indices=indices_plus)
+        probs_minus, pred_lbls_minus = cls.computeProbabilities(results_minus, is_statevector, n_data, n_classes,
+                                                                indices=indices_minus)
         costs_plus = cost_estimate(probs_plus, labels)
         costs_minus = cost_estimate(probs_minus, labels)
 
         costs_curr = None
         if (results_curr is not None):
-            probs_curr, pred_lbls_curr = cls.computeProbabilities(results_curr, is_statevector, n_data, n_classes, indices=indices_curr)
+            probs_curr, pred_lbls_curr = cls.computeProbabilities(results_curr, is_statevector, n_data, n_classes,
+                                                                  indices=indices_curr)
             costs_curr = cost_estimate(probs_curr, labels)
         else:
             costs_curr = costs_minus
 
         g_spsa = (costs_plus - costs_minus) * delta / (2.0 * get_c_spsa(iteration, optimizer_params))
+
         # updated theta
         thetas = thetas - get_a_spsa(iteration, optimizer_params) * g_spsa
 
@@ -154,7 +152,8 @@ class SPSAOptimizer():
         for i in range(n_data):
             if is_statevector:
                 if indices is not None:
-                    temp = results.get_statevector(int(indices[i])) # cast to int, otherwise int32 will not be recognized as int
+                    temp = results.get_statevector(
+                        int(indices[i]))  # cast to int, otherwise int32 will not be recognized as int
                 else:
                     temp = results[i].get_statevector()
                 outcome_vector = (temp * temp.conj()).real
@@ -187,16 +186,20 @@ class SPSAOptimizer():
 
         return predicted_probs, predicted_labels
 
+
 def get_a_spsa(iteration, optimizer_params):
     c0, c1, c2, c3, c4 = optimizer_params
     return float(c0) / np.power(iteration + 1 + c4, c2)
+
 
 def get_c_spsa(iteration, optimizer_params):
     c0, c1, c2, c3, c4 = optimizer_params
     return float(c1) / np.power(iteration + 1, c3)
 
+
 def gen_delta(size):
     return 2 * np.random.default_rng().integers(2, size=size) - 1
+
 
 def generateDirections(iteration, thetas, optimizer_params):
     c_spsa = get_c_spsa(iteration, optimizer_params)
