742d0ea69579334b8da18b732520ef5feaeaec1e
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-05-15 18:33:07 +0200

    Add IA to start Rust App

---
+++ 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/RustApp_Start_IA/files/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install unzip
+
+echo "Searching for DA containing Rust application"
+IFS=';' read -ra FILES <<< "$DAs"
+for i in "${FILES[@]}"; do
+    IFS=',' read -ra ENTRY <<< "$i"
+    if [[ ( -f $CSARROOT${ENTRY[1]} ) && ( ${ENTRY[1]} == *.zip ) ]]; then
+        sudo unzip $CSARROOT${ENTRY[1]} -d ~/app
+        break
+    fi
+done
+cd ~/app
+echo "Found application comprises the following files:"
+ls
+
+echo "Starting application..."
+sudo cargo run
