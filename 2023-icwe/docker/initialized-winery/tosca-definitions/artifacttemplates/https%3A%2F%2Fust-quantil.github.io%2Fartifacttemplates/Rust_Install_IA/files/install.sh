47f1f4d523792e822b6a74f4fb4c100b3d64ba1c
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-05-16 16:57:23 +0200

    Eventually fix Rust install

--- 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/Rust_Install_IA/files/install.sh
+++ 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/Rust_Install_IA/files/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update >/dev/null
-sudo apt-get install -yqq curl >/dev/null
+sudo apt-get update
 echo "Installing Rust..."
-curl https://sh.rustup.rs -sSf -o rust_install.sh
-sudo chmod +x rust_install.sh
-sudo ./rust_install.sh -y >> ./rust_install.log
+sudo apt-get install -yqq rustc cargo
 echo "Rust successfully installed"
