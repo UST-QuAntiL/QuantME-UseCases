911b7570b6b8db45932f56113a26422845b47bb6
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-05-16 15:39:08 +0200

    Update Rust install script

--- 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/Rust_Install_IA/files/install.sh
+++ 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/Rust_Install_IA/files/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -yqq rustc
+sudo apt-get update >/dev/null
+sudo apt-get install -yqq curl >/dev/null
+echo "Installing Rust..."
+curl https://sh.rustup.rs -sSf -o rust_install.sh
+sudo chmod +x rust_install.sh
+sudo ./rust_install.sh -y >> ./rust_install.log
+echo "Rust successfully installed"
