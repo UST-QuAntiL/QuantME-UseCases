a51305326abc1d5931a2b94db078b4577e66cc6a
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-05-17 11:06:04 +0200

    Fix extraction of RustApp DAs

--- 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/RustApp_Start_IA/files/start.sh
+++ 2023-icwe/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/RustApp_Start_IA/files/start.sh
@@ -4,10 +4,15 @@ sudo apt-get update
 sudo apt-get install -yqq unzip
 
 echo "Searching for DA containing Rust application"
+echo "DA string: $DAs"
+CSARROOT=$(find . -maxdepth 1 -path "*.csar");
+echo "CSAR root: $CSARROOT"
 IFS=';' read -ra FILES <<< "$DAs"
 for i in "${FILES[@]}"; do
     IFS=',' read -ra ENTRY <<< "$i"
+    echo "Found DA path: $ENTRY[1]"
     if [[ ( -f $CSARROOT${ENTRY[1]} ) && ( ${ENTRY[1]} == *.zip ) ]]; then
+        echo "Found DA to unzip: $CSARROOT${ENTRY[1]}"
         sudo unzip $CSARROOT${ENTRY[1]} -d ~/app
         break
     fi
