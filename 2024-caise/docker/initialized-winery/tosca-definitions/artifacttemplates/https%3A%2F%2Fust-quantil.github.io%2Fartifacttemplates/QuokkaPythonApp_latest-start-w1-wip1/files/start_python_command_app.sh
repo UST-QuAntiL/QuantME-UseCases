8ea7857c9e551fc8096ab2fdebc2c92a45d9a1c6
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-11-18 15:07:09 +0100

    Start Quokka Apps based on opened ports

--- 2024-caise/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QuokkaPythonApp_latest-start-w1-wip1/files/start_python_command_app.sh
+++ 2024-caise/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QuokkaPythonApp_latest-start-w1-wip1/files/start_python_command_app.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-echo 'Name:'$Name
-echo 'StartupCommand:'$StartupCommand
+echo 'Name: '$Name
+echo 'StartupCommand: '$StartupCommand
+echo 'Port: '$VMOpenPorts
+vmPort="${VMOpenPorts%%,*}"
+
 if [[ $StartupCommand == nohup* ]]; then
 	echo "Starting App using existing startup command..."
+	if [[ -z "${vmPort}" ]]; then
+		echo 'Starting on default port...'
+		echo $StartupCommand >> ~/$Name/startup.sh
+	else
+		echo 'Starting on specified port: '
+		echo $StartupCommand ' --port=' $vmPort >> ~/$Name/startup.sh
+	fi
 	echo $StartupCommand >> ~/$Name/startup.sh
 else
 	echo "Starting App using modifying using nohup..."
-	echo 'nohup ' $StartupCommand  ' > log.log 2>&1 &'  >> ~/$Name/startup.sh
+	if [[ -z "${vmPort}" ]]; then
+		echo 'Starting on default port...'
+		echo 'nohup ' $StartupCommand ' > log.log 2>&1 &'  >> ~/$Name/startup.sh
+	else
+		echo 'Starting on specified port: '
+		echo 'nohup ' $StartupCommand ' --port=' $vmPort ' > log.log 2>&1 &'  >> ~/$Name/startup.sh
+	fi
 fi
-
 
 (cd ~/$Name/ && nohup sh startup.sh &)
 echo 'Started'
 sleep 5
-
-
-
