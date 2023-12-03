68c3b122d424b17afe60daa81acf04718a679dd9
mbeisel <beiselmn@gmail.com>
2023-12-03 14:25:15 +0100

    modify startupcommand and updated optimizationservice sources

--- 2024-caise/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QuokkaPythonApp_latest-start-w1-wip1/files/start_python_command_app.sh
+++ 2024-caise/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QuokkaPythonApp_latest-start-w1-wip1/files/start_python_command_app.sh
@@ -18,12 +18,32 @@ if [[ $StartupCommand == nohup* ]]; then
 else
 	echo "Starting App using modifying using nohup..."
 	if [[ -z "${vmPort}" ]]; then
-		echo 'Starting on default port...'
-		echo 'nohup ' $StartupCommand ' > log.log 2>&1 &'  >> ~/$Name/startup.sh
-	else
-		echo 'Starting on specified port: '
-		echo 'nohup ' $StartupCommand ' --port=' $vmPort ' > log.log 2>&1 &'  >> ~/$Name/startup.sh
-	fi
+      echo 'Starting on default port...'
+      # Split the StartupCommand into an array of commands using '&&' as the delimiter
+      IFS='&&' read -ra commands <<< "$StartupCommand"
+
+      # Get the last command in the array and inject 'nohup'
+      last_command=${commands[-1]}
+      nohup_last_command="nohup $last_command"
+
+      # Reconstruct the StartupCommand with 'nohup' injected before the last command
+      new_command=$(IFS='&&' ; echo "${commands[@]:0:${#commands[@]}-1} && $nohup_last_command")
+
+      echo "$new_command" ' > log.log 2>&1 &' >> ~/$Name/startup.sh
+  else
+      echo 'Starting on specified port: '
+      # Split the StartupCommandElse into an array of commands using '&&' as the delimiter
+      IFS='&&' read -ra commands_else <<< "$StartupCommand"
+
+      # Get the last command in the array and inject 'nohup'
+      last_command_else=${commands_else[-1]}
+      nohup_last_command_else="nohup $last_command_else"
+
+      # Reconstruct the StartupCommandElse with 'nohup' injected before the last command
+      new_command_else=$(IFS='&&' ; echo "${commands_else[@]:0:${#commands_else[@]}-1} && $nohup_last_command_else")
+
+      echo "$new_command_else --port=$vmPort" ' > log.log 2>&1 &' >> ~/$Name/startup.sh
+  fi
 fi
 
 (cd ~/$Name/ && nohup sh startup.sh &)
