20f60d6fd2124180a93339a1db47ff51902e8f07
mbeisel <beiselmn@gmail.com>
2024-04-18 15:20:07 +0200

    adapt qiskit install.sh

--- 2024-icwe-tutorial/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/Qiskit_Install-latest-w1/files/install.sh
+++ 2024-icwe-tutorial/docker/initialized-winery/tosca-definitions/artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/Qiskit_Install-latest-w1/files/install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -qq
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip -qq
-	
+
 pip3 install --upgrade pip
-	
-pip install qiskit
+
+pip install qiskit==0.44.1
