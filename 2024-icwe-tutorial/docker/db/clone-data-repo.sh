eac455f882124cb40ead1eeb54b01bd11ce9af78
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2024-04-26 11:17:37 +0200

    Fix redundant repo clone

--- 2024-icwe-tutorial/docker/db/clone-data-repo.sh
+++ 2024-icwe-tutorial/docker/db/clone-data-repo.sh
@@ -9,12 +9,3 @@ if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${QC_ATLAS_SUBFOLDER_CONTENT_BACKUP
 else
 	    echo "unable to find specified directory with example data for qc-atlas in the repository"
 fi
-
- # clone repo, if successful copy setup script to /docker-entrypoint-initdb.d/
-git clone --single-branch --branch ${PATTERNATLAS_CONTENT_REPOSITORY_BRANCH} ${PATTERNATLAS_CONTENT_REPOSITORY_URL} ${PATTERNATLAS_CONTENT_REPOSITORY_PATH}
-if [ -d "${PATTERNATLAS_CONTENT_REPOSITORY_PATH}/${PATTERNATLAS_SUBFOLDER_CONTENT_BACKUP_FILES}" ]; then
-    cp setup-patternatlas.sh /docker-entrypoint-initdb.d/
-    echo "${PATTERNATLAS_CONTENT_REPOSITORY_BRANCH} of pattern-atlas-content repo was cloned successfully"
-else
-    echo "unable to find specified directory with example data for pattern-atlas in the repository"
-fi
